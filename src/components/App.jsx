b34bef2085a13158704a327c408ff12f86e24f33
yanchenko04 <yanchenko0405@gmail.com>
2022-11-22 21:42:40 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React from "react";
+
+
+export class App extends React.Component{
+  state = {
+    contacts: [],
+    name: ''
+  }
+
+
+
+  render(){
+    return(
+      <div>
+        <input
+  type="text"
+  name="name"
+  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+  required
+/>
+      </div>
+    )
+  }
 };
